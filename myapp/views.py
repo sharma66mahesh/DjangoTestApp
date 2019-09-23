6935532c80db4e6be7779692a8c924e562b8a662
Prajesh777 <sity1n117014@islingtoncollege.edu.np>
2019-09-23 08:16:48 +0545

    Views Modified

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from .models import Post
+from django.views.generic.list import ListView
 
 # Create your views here.
 
+class IndexView(ListView):
+    template_name = "myapp/index.html"
+    context_object_name = "post_list"
+
+    def get_queryset(self):
+        return Post.object.all
 
 
 
